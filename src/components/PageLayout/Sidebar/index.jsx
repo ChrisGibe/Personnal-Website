463cd529c68dabcfb0cf31d514e158c5aac349ac
ChrisGibe <gibelli.christophe01@gmail.com>
2021-01-27 14:33:56 +0100

    Uptade + test Disabling GA

--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -15,6 +15,9 @@ const {
   stackOverflow, github, twitter,
 } = Config.social;
 
+const disable = () => {
+  window['ga-disable-UA-123456-1'] = true;
+};
 const DomContent = () => (
   <aside>
     <div className={style.profileAvatar} />
@@ -62,6 +65,7 @@ const DomContent = () => (
           </a>
         </li>
       </ul>
+      <button type="button" onClick={disable}>Disable</button>
       <div className={style.resumeDownload}>
         <a href="../resume.pdf" download target="_blank">Télécharger CV</a>
       </div>
